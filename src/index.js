d489c23b82fd2c67ff59165e6d6246631109d105
vischio <rns11aiwbk@libero.it>
2021-11-29 20:11:52 +0100

    finish webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import printMe from './print.js';
+import './style.css';
+
 function component() {
     const element = document.querySelector('#container');
+    element.classList.add('hello');
+
+    const btn = document.createElement('button');
   
     element.innerHTML = 'prova webpack ';
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
   
     return element;
   }
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    devServer: {
+      static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },  
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+};
